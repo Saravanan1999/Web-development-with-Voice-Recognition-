630c18f9d77610d046b712dae186e4f40dafbdca
Saravanan1999 <46484761+Saravanan1999@users.noreply.github.com>
2020-04-02 17:31:12 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="js/browser.js"></script>
+<script src="clickbtn.js"></script>
+<script src='voicerecon.js'></script>
+</head>
+<body onload="clickbtn()" onload="detectbrow()">
+        <a href="title.html">Home </a><br><br>
+        <button type="button" class="btn btn btn-outline-success my-2 my-sm-0" onclick="recon('home')" id='speak'>Speak/Read</button>
+    
+    </body>
+    <textarea id="results" class="form-control" cols="80" rows="5" ></textarea>
+    </html>
---
+++ main.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="js/browser.js"></script>
+<script src="clickbtn.js"></script>
+<script src='voicerecon.js'></script>
+<link href=homestyle.css rel="stylesheet" type="text/css"/>
+</head>
+<body onload="clickbtn()" onload="detectbrow()">
+<div class="logo">
+      <h3 style="font-size:20px"><p><span style='color: Black'>Greivance</span> <span style='color:Black'>Redressal  
+      
+    </div>
+    <br>
+    <div class="wel">
+
+</div><hr>
+<br><br>
+<div class="flex-wrapper">
+  <div class="container">
+      <a href="a/login.php" style="color:black"><div id="left"><br><br><br>
+      <img src="Complaint.png" width='170'><br><b><p style="font-size:20px">Complainant</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+        </div></a>
+        
+      <a href="b/login.php" style="color:black"><div id="right"  ><br><br><br>
+      <br><img src="expert.jpg" width='160'><b><p style="font-size:20px">Expert</b></p>
+      </div></a>
+      <div class="div.footer-section contact-form"></div>
+    </div>
+</div>
+<br><br><br><br>
+    <div class="footer" style="background:white;color:black">
+      &copy;GREIVANCE REDRESSAL &nbsp;&nbsp;
+      
+    </div>
+       
+    
+    </body><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <button type="button" class="btn btn btn-outline-success my-2 my-sm-0" onclick="recon('home')" id='speak'>Speak/Read</button>
+    <textarea id="results" class="form-control" cols="80" rows="5" ></textarea>
+    </html>
